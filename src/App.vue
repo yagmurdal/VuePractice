9220cc2cccb5c69dbad868777f45ab9edad98f51
Yagmur Dal <nyagmurdal@gmail.com>
2022-01-29 19:41:06 +0300

    ColorFlipper

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from "./components/Counter.vue";
 import Counter from "./components/Counter.vue";
 import Todo from "./components/Todo.vue";
+import ColorFlipper from "./components/ColorFlipper.vue";
 </script>
 
 <template>
   <h1>Mini Projects with Vue 3.2</h1>
   <h2>Counter</h2>
   <Counter />
-  <hr>
+  <hr />
   <h2>To Do</h2>
   <Todo />
-  <hr>
+  <hr />
+  <h2>Color Flipper</h2>
+  <ColorFlipper />
+  <hr />
 </template>
 
 <style>
---
+++ src/components/ColorFlipper.vue
@@ -0,0 +1,21 @@
+<!-- js  -->
+<script setup>
+import { ref } from 'vue';
+const color = ref('')
+
+
+
+
+</script>
+
+<!-- html -->
+<template>
+    <div :style="{ backgroundColor: color }">
+        <input type="text" v-model="color" />
+    </div>
+</template>
+
+<!-- css -->
+<style scoped>
+</style>
+
