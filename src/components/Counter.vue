7b03e234fc46bbb3f7c516e5353e5368a215567a
Yagmur Dal <nyagmurdal@gmail.com>
2022-01-29 16:36:30 +0300

    counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,27 @@
+<!-- js  -->
+<script setup>import { ref } from 'vue';
+
+    const number = ref(0)
+
+    const inc = () => {
+        number.value ++
+    }
+</script>
+
+<!-- html -->
+<template>
+    <button @click="inc">Click</button>
+    <span>{{number}}</span>
+</template>
+
+<!-- css -->
+<style scoped>
+    button{
+        font-size: 20px;
+        margin-right: 20px;
+    }
+    span{
+        font-size: 20px;
+    }
+</style>
+
